550b42957d341bd01bf0ba99b672f67da76e0553
B0ySetsF1re <montersberg@gmail.com>
2020-08-21 00:22:10 +0300

    created "paginationViewController", which will manage pagination view behaviour

---
+++ controllers/paginationViewController.js
@@ -0,0 +1,15 @@
+// More likely it will be class, which will be exported as a module, with which userController (and others if needed) will work to manage pages view
+
+function visibilityAreaPresetsLength(total) {
+  /* - TBD - */
+}
+
+exports.genVisibilityAreaPresets = async function(total) {
+
+  /* - TBD - */
+
+}
+
+exports.checkPagePos = function(page, total) {
+  /* - TBD - */
+}
