6dc20cf7fd5bcd386183cc2a795a0bf626bac50a
B0ySetsF1re <montersberg@gmail.com>
2020-08-21 22:14:55 +0300

    implemented paginationViewController to manage presets, but either will not use it or modify will modify it

--- controllers/paginationViewController.js
+++ controllers/paginationViewController.js
@@ -1,15 +1,57 @@
-// More likely it will be class, which will be exported as a module, with which userController (and others if needed) will work to manage pages view
+class Pagination {
+  constructor() {
+    this.visAreaPresets = null;
+    this.genAreaPresetsCompleted = null;
+  }
 
-function visibilityAreaPresetsLength(total) {
-  /* - TBD - */
+  initVisAreaArray(total) {
+    return new Promise(resolve => {
+      if(total > 1 && total == 2) {
+        this.visAreaPresets = new Array(0);
+        this.visAreaPresets[0] = new Array(2);
+
+        resolve();
+      } else {
+        this.visAreaPresets = new Array(total - 2);
+
+        for(let i = 0; i < total - 2; i++) {
+          this.visAreaPresets[i] = new Array(3);
+        }
+
+        resolve();
+      }
+    });
+  }
+
+  putVisAreaVals(totalPresets, totalPgs) {
+    return new Promise(resolve => {
+
+      for(let i = 0; i < totalPresets; i++) {
+        for(let j = 0; j < totalPgs; j++) {
+          this.visAreaPresets[i][j] = i + j + 1;
+        }
+      }
+
+      resolve(this.visAreaPresets);
+    });
+  }
+
+  async genVisAreaPresets(total) {
+
+    await this.initVisAreaArray(total);
+
+    if(total > 1 && total == 2) {
+      await this.putVisAreaVals(this.visAreaPresets.length, 2)
+    } else {
+      await this.putVisAreaVals(total - 2, 3);
+    }
+
+    console.log(this.visAreaPresets);
+  }
+
+  async getPagePos(page) {
+    /* - TBD - */
+  }
 }
 
-exports.genVisibilityAreaPresets = async function(total) {
-
-  /* - TBD - */
-
-}
-
-exports.checkPagePos = function(page, total) {
-  /* - TBD - */
-}
+module.exports = Pagination;
