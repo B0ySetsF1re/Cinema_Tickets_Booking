fa976dc2118135b3feb423a188b3b7dadfbec377
B0ySetsF1re <montersberg@gmail.com>
2020-09-02 13:07:31 +0300

    implemented modals for non selected action and users delete confirmation

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -21,21 +21,52 @@ function getRegFormContent() {
   xhttp.send(); // Sending content to the dashboard page
 }
 
-function checkManageTabAction(e) {
-  e.preventDefault();
+function usersSelected() {
+  let usersTable = document.getElementById('manageTabUsersTable');
 
-  console.log('Action executed...');
-  // document.getElementById('manageTabForm').submit(); // Testing submition after processing code
+  for(let i = 1; i < usersTable.rows.length; i++) {
+    if(usersTable.rows.item(i).getElementsByTagName('input')[0].checked) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
-function manageConfirmRemove() {
- /* TBD */
-}
-
-function manageSelectNewRoles() {
+function manageTabSelectNewRoles() {
   /* TBD */
 }
 
+function checkManageTabAction(e) {
+  e.preventDefault();
+  let selectVal = document.getElementById('manageTabFormSelect').value;
+  let manageTabForm = document.getElementById('manageTabForm');
+
+  $(document).ready(function() {
+    if(selectVal == 'Select action...') {
+      $('#selectActionErrorModal').modal('show');
+    } else if(selectVal == 'Delete') {
+      if(usersSelected()) {
+        $('#deleteUsersModal').modal('show');
+
+        document.getElementById('deleteUsersModalConfirmBtn').addEventListener('click',
+        () => { manageTabForm.submit(); }, false);
+      } else {
+        $('#deleteUsersErrorModal').modal('show');
+      }
+    } else if(selectVal == 'Change role') {
+      console.log('Selected "' + selectVal + '" action...');
+    } else if(selectval == 'Export') {
+      console.log('Selected "' + selectVal + '" action...');
+    } else {
+      console.log('Selected "' + selectVal + '" action...');
+    }
+  });
+
+}
+
 window.onload = function() {
-  document.getElementById('manageTabForm').addEventListener('submit', checkManageTabAction, false);
+  let manageTabForm = document.getElementById('manageTabForm');
+
+  (manageTabForm != null) ? manageTabForm.addEventListener('submit', checkManageTabAction, false) : '';
 }
