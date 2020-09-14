4b0e7cbf328250245e74f954758da6bd16bb28e6
B0ySetsF1re <montersberg@gmail.com>
2020-09-14 18:47:13 +0300

    removed old comments

--- models/roleModel.js
+++ models/roleModel.js
@@ -1,21 +1,3 @@
-/*let roles = {
-  admin: {
-    can: ['create_roles', 'manage_users'],
-    inherits: ['manager']
-  },
-  manager: {
-    can: ['manage_orders', 'manage_archive', 'comment']
-  },
-  basic: {
-    can: ['comment', {
-      name: 'edit',
-      when: function (params) {
-        return params.user.id === params.comment.owner;
-      }
-    }]
-  }
-}*/
-
 let roles = {
   admin: {
     can: ['create_roles', 'manage_users'],
