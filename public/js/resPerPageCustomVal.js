0e86e9892cf2caafeb3231a6e9bd178a43201095
B0ySetsF1re <montersberg@gmail.com>
2020-09-28 20:57:57 +0300

    turned old "setResPerPage" function as a separated module

---
+++ public/js/resPerPageCustomVal.js
@@ -0,0 +1,19 @@
+function setResPerPage(e, input) {
+  if(e.key == 'Enter') {
+    let regExp = /^\d+$/;
+
+    if(input.value == '' || regExp.test(input.value) == false) {
+      window.alert('This field can\'t be empty and should contain numbers only!');
+    } else {
+      let currentUrl = window.location.href;
+      let pageNumExp = RegExp('[/]\\d+', 'g');
+
+      pageNumExp.test(currentUrl);
+      currentUrl = currentUrl.substring(0, pageNumExp.lastIndex);
+
+      window.location.replace(currentUrl + '?resPerPage=' + parseInt(input.value));
+    }
+  }
+}
+
+export { setResPerPage };
